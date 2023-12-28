c9f01bd3f36422fa6765d9dbbd7cdb48a03a21e8
Juliana Senatore <jusenatore@gmail.com>
2023-12-28 22:49:49 +0100

    corrigindo tudo

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ function verificarChute() {
         if (chute > numeroSecreto) { 
             exibirTextoNaTela('p', 'O número secreto é menor');
         } else {
-            exibirTextoNaTela('p', 'O número secreto é major');
+            exibirTextoNaTela('p', 'O número secreto é maior');
         }
         tentativas++;
         limparCampo();
